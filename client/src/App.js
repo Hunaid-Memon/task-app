e8da114fa8e17ab4b7ed9a861a872a065bfd0dc2
Hunaid <61511895+Hunaid-Memon@users.noreply.github.com>
2022-01-20 23:34:27 +0500

    Updated till alerts

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,34 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import TaskState from './components/context/task/TaskState';
+import AuthState from './components/context/auth/AuthState';
+import AlertState from './components/context/alert/AlertState'
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 
 const App = () => {
   return (
-    <TaskState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </TaskState>
+    <AuthState>
+      <TaskState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </TaskState>
+    </AuthState>
   );
 }
 
---
+++ client/src/components/context/alert/AlertState.js
@@ -0,0 +1,42 @@
+import { useReducer } from 'react';
+import alertContext from './alertContext';
+import alertReducer from './alertReducer';
+// const { nanoid } = require("nanoid");
+import { v4 as uuidv4 } from 'uuid';
+
+import {
+    SET_ALERT,
+    REMOVE_ALERT
+} from '../types';
+
+const AlertState = props => {
+    const initialState = []
+
+    const [state, dispatch] = useReducer(alertReducer, initialState);
+
+    // Set Alert
+    const setAlert = (msg, type, timeout = 5000) => {
+        // const id = nanoid();
+        // const id = 2
+        const id = uuidv4();
+        dispatch({ type: SET_ALERT, payload: { msg, type, id } });
+
+        setTimeout(()=>{
+            dispatch({ type: REMOVE_ALERT, payload: id })
+        }, timeout)
+    }
+
+    return (
+        <alertContext.Provider
+            value={{
+               alerts: state,
+               setAlert
+            }}
+        >
+            {props.children}
+        </alertContext.Provider>
+    )
+
+}
+
+export default AlertState;
