f6f92deda862974435fbe00a8566ebfb1a6d2022
Hunaid <61511895+Hunaid-Memon@users.noreply.github.com>
2022-01-23 01:19:40 +0500

    Updated users

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import TaskFilter from '../tasks/TaskFilter';
 import TaskForm from '../tasks/TaskForm';
 import Tasks from '../tasks/Tasks'
+import AuthContext from '../context/auth/authContext';
 
 const Home = () => {
+    const authContext = useContext(AuthContext);
+
+    useEffect(() => {
+        authContext.loadUser();
+        // eslint-disable-next-line
+    }, []);
+
     return (
         <div className='grid-2' >
             <div>
--- routes/auth.js
+++ routes/auth.js
@@ -45,7 +45,7 @@ router.post('/', [
         const isMatch = await bcrypt.compare(password, user.password);
         
         if(!isMatch) {
-            return res.status(400).json({ msg: 'Incorrect Password' });
+            return res.status(400).json({ msg: 'Incorrect Password.' });
         }
 
         const payload = {
